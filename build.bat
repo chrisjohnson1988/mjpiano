d8ff93121347a7ab89a1e19427645d8df47e1b3d
Christopher Johnson <cjohn@ceh.ac.uk>
2012-11-04 19:29:02 +0000

    Using compass as it was intended

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-java -jar build-lib/StaticWeb.1.0-SNAPSHOT-Combined.jar
+java -Dgem.path="build-lib\\gems" -jar build-lib/StaticWeb.1.0-SNAPSHOT-Combined.jar
 pause
--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,2 @@
-java -jar build-lib/StaticWeb.1.0-SNAPSHOT-Combined.jar "deploy"
+java -Dgem.path="build-lib\\gems" -jar build-lib/StaticWeb.1.0-SNAPSHOT-Combined.jar "deploy"
 pause
