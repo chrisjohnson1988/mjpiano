789b264b1399f1cc38e6557e4451ddcd59631b65
Christopher Johnson <chrisajohnson1988@gmail.com>
2018-02-13 18:22:37 +0000

    Enable google analytics

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-25043089-1', 'auto');
+ga('send', 'pageview');
+
 function focusedSection() {
   var pos = $(document).scrollTop() + $(window).height()/2;
   var sections = $('section')
@@ -20,6 +28,7 @@ function setSelected(section) {
     history.replaceState(undefined, undefined, "#" + section);
   }
   $('body').attr('data-selected', section);
+  ga('send', 'pageview', section);
 }
 
 $(document).ready(function() {
